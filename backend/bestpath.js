332d61719c6c45614bfc948b4c693d493883e084
David Gu <dgu42@gatech.edu>
2024-10-19 20:09:30 -0400

    I David Gu finished the backend stuff

--- backend/bestpath.js
+++ backend/bestpath.js
@@ -4,22 +4,29 @@ async function findBestPaths(apiKey, origin, destination) {
     const endpoint = 'https://maps.googleapis.com/maps/api/directions/json';
     
     const departure_time = new Date();
-    departure_time.setHours(0, 0, 0, 0);
+    departure_time.setHours(3, 0, 0, 0);
 
     const params = {
         origin: origin,
         destination: destination,
         trafficModel: "OPTIMISTIC", // Get the best route based on best traffic condition
-        departure_time: departure_time, // Get the best route based on 12am time (so no traffic time)
+        departureTime: departure_time.toISOString(), // Get the best route based on 12am time (so no traffic time)
         alternatives: true, // Get multiple routes
+        routeModifiers: {
+            avoidTolls: false,
+            avoidHighways: false,
+            avoidFerries: false,
+            avoidIndoor: false
+        },
+
         key: apiKey
     };
-
+    
     try {
         const response = await axios.get(endpoint, { params });
         if (response.data.status === 'OK') {
-            const route = response.data.routes[0];
-            return route;
+            const routes = response.data.routes;
+            return routes;
         } else {
             console.error(`Error: ${response.data.status}`);
             return null;
@@ -30,6 +37,50 @@ async function findBestPaths(apiKey, origin, destination) {
     }
 }
 
+async function countTurns(route) {
+    if (route) {
+        let turns = 0;
+        for (let i = 0; i < route.legs.length; i++) {
+            for (let j = 0; j < route.legs[i].steps.length; j++) {
+                if (route.legs[i].steps[j].maneuver && route.legs[i].steps[j].maneuver.includes('turn')) {
+                    turns++;
+                }
+            }
+        }
+        return turns;
+    } else {
+        return null;
+    }
+}
+
+async function getLeastTurnsPath(routes) {
+    if (routes) {
+        let leastTurns = routes[0];
+        let minCount = countTurns(routes[0]);
+        for (let i = 1; i < routes.length; i++) {
+            let t = countTurns(routes[i]);
+            if (t < minCount) {
+                leastTurns = routes[i];
+                minCount = t;
+            }
+        }
+        return leastTurns;
+    } else {
+        return null;
+    }
+}
+
+async function getBestPath(apiKey, origin, destination) {
+    return findBestPaths(apiKey, origin, destination)
+        .then(routes => {
+            if (routes) {
+                return getLeastTurnsPath(routes);
+            } else {
+                return null;
+            }
+        });
+}
+
 module.exports = {
-    findBestPath
+    getBestPath
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
+const bestpath = require('./bestpath.js');
+
 require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
-console.log(apiKey);
-
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
+
+app.length('/bestpath', (req, res) => {
+    const origin = req.query.origin;
+    const destination = req.query.destination;
+
+    bestpath.getBestPath(apiKey, origin, destination)
+        .then(route => {
+            if (route) {
+                res.json(route);
+            } else {
+                res.status(404).send('No routes found.');
+            }
+        })
+        .catch(error => {
+            res.status(500).send('Error:', error);
+        });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
